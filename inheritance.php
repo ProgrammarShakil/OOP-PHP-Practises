d5bb5988b5493314814d9be1f4ed865a28d43a72
shakil islam <freelancershakil913@gmail.com>
2021-09-22 20:31:30 +0600

    without constructor

--- inheritance.php
+++ inheritance.php
@@ -2,11 +2,12 @@
 class parents{
     public $name;
     public $name2;
-    public $color;
+    public $age;
 
-    public function it_will_be_inherit($name)
+    public function it_will_be_inherit($name,$age)
     {
-     return  $this->name = $name;
+     echo  $this->name = $name;
+     echo  $this->age = $age;
     }
 }
 
@@ -14,12 +15,25 @@ class son extends parents{
     // public function inherited($name){
     //     return $this->name2=$name;
     // }
+    public $plus;
+    public function it_will_be_inherited($name,$age,$plus)
+    
+    {
+     echo  $this->name = $name;
+     echo  $this->age = $age;
+     echo  $this->plus = $plus;
+    }
     
 }
 
-$parents = new son();
-echo $parents->it_will_be_inherit("baba_ma"); // inherited properties + method
-// echo $parents->inherited("baba_ma");  // inherited just properties
+   $parents = new parents();
+   $parents->it_will_be_inherit("baba_ma"," 35");  // inherited properties + method
+   $son = new son();
+   $son->it_will_be_inherited(" son"," 45",30);     // inherited properties + method
+
+// echo $parents->inherited("baba_ma");                       // inherited just properties
+
+
 
 
 
