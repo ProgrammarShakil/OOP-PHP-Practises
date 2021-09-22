eec6ac904d3fad75cbe1125b77f4fa42e6abdb86
shakil islam <freelancershakil913@gmail.com>
2021-09-22 16:47:24 +0600

    destruct

--- constructor_destructor.php
+++ constructor_destructor.php
@@ -8,6 +8,15 @@ class student{
     {
        echo $this->name = $n;
     }
+    public function __destruct()
+     // when all funtion will be execute in this file then __destruct will be auto executed
+     // if we need to auto close anything when all work has been done...we can use destruct...
+     // as like we need to close connection , when we done all works...
+    {
+       echo " All Work has been done {$this->name}";
+   
+
+    }
 }
 
 new student("shakil"); // auto call when you creat a object
