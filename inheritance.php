57085c688339e54dee42d7e7aedeeb61282dc7d1
shakil islam <freelancershakil913@gmail.com>
2021-09-22 21:02:18 +0600

    final inheritance

--- inheritance.php
+++ inheritance.php
@@ -1,40 +1,36 @@
 <?php 
-class parents{
+class Teacher{
     public $name;
-    public $name2;
-    public $age;
+    public $salary;
 
-    public function it_will_be_inherit($name,$age)
+    public function __construct($n,$s)
     {
-     echo  $this->name = $name;
-     echo  $this->age = $age;
+        $this->name = $n;
+        $this->salary = $s;
+    }
+
+    public function info()
+    {
+       echo $this->name;
+       echo $this->salary;
     }
 }
 
-class son extends parents{
-    // public function inherited($name){
-    //     return $this->name2=$name;
-    // }
-    public $plus;
-    public function it_will_be_inherited($name,$age,$plus)
-    
+class Headmaster extends Teacher{
+    public $extra = 10000;
+
+    public function info()
     {
-     echo  $this->name = $name;
-     echo  $this->age = $age;
-     echo  $this->plus = $plus;
+            $this->extra += $this->salary;
+       echo $this->name;
+       echo $this->extra;
     }
-    
 }
 
-   $parents = new parents();
-   $parents->it_will_be_inherit("baba_ma"," 35");  // inherited properties + method
-   $son = new son();
-   $son->it_will_be_inherited(" son"," 45",30);     // inherited properties + method
-
-// echo $parents->inherited("baba_ma");                       // inherited just properties
-
-
-
+$teacher = new Teacher("rohim "," 20000");
+$teacher->info();
+$headmaster = new Headmaster(" kuddus "," 20000");
+$headmaster->info();
 
 
 ?>
